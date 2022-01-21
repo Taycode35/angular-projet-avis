a6c0a84d6fbc3f65e174a13874d91d88b582d0d2
andrick <andricktaylor1@gmail.com>
2022-01-21 15:53:44 +0100

    1.Création des services. 2.Création des composants. 3.Installation de Bootstrap et JQuery

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from './home/home.component';
+import { ProfilComponent } from './profil/profil.component';
+import { BoardAdminComponent } from './board-admin/board-admin.component';
+import { BoardModeratorComponent } from './board-moderator/board-moderator.component';
+import { BoardPlayerComponent } from './board-player/board-player.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent,
+    ProfilComponent,
+    BoardAdminComponent,
+    BoardModeratorComponent,
+    BoardPlayerComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/board-player/board-player.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-board-player',
+  templateUrl: './board-player.component.html',
+  styleUrls: ['./board-player.component.css']
+})
+export class BoardPlayerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
